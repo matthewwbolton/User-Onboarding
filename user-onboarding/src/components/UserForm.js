0ba207319ed7f07d740ee7c32cb7799e13834072
Matthew Bolton <matthewwbolton@gmail.com>
2020-02-27 14:49:17 -0500

    updated form to include validation with Yup

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -4,18 +4,21 @@ import * as Yup from 'yup';
 import axios from 'axios';
 
 
-const UserForm = () => {
+const UserForm = ({values, errors, touched}) => {
     return (
         <div>
             <Form>
                 <label htmlFor='name'>Name:</label>
-                <Field id='name' type='text' name='name' placeholder='Name' />
+                <Field id='name' type='text' name='name' placeholder='Name' value={values.name} />
+                {touched.name && errors.name && <p>{errors.name}</p>}
                 <label htmlFor='eamil'>Email:</label>
-                <Field id='email' type='email' name='email' placeholder='Email'  />
+                <Field id='email' type='email' name='email' placeholder='Email' value={values.email}  />
+                {touched.email && errors.email && <p>{errors.email}</p>}
                 <label htmlFor='password'>Password:</label>
-                <Field id='password' type='password' name='password' placeholder='Password'  />
+                <Field id='password' type='password' name='password' placeholder='Password' value={values.password}  />
+                {touched.password && errors.password && <p>{errors.password}</p>}
                 <label htmlFor='tos'>Terms of Service</label>
-                <Field id='tos' type='checkbox' name='tos' />
+                <Field id='tos' type='checkbox' name='tos' value={values.tos}/>
                 <button type='submit'>Submit</button>
             </Form>   
         </div>
@@ -31,6 +34,18 @@ const FormikUserForm = withFormik({
         tos: tos ||  false
        }
     },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .required('This field is required'),
+        email: Yup.string()
+            .email()
+            .required('This field is required'),
+        password: Yup.string()
+            .min(6)
+            .required('You must enter a valid password')  
+
+    }),
 
     handleSubmit(values){
         console.log(values);
